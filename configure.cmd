cf1c0f19757a8e7b3f2a12b4a6602a0bcde278b6
KO Myung-Hun <komh@chollian.net>
2016-09-26 14:04:10 +0900

    my: add build scripts for OS/2
        new file:   configure.cmd

---
+++ configure.cmd
@@ -0,0 +1,9 @@
+extproc sh
+
+d=$(dirname "$0" | tr '\\' /)
+
+n=configure
+test -f "$d/$n." || { echo "\`$d/$n' not found !!!"; exit 1; }
+
+opts="--enable-shared --enable-static"
+"$d/$n" $opts "$@" 2>&1 | tee "$n.log"
